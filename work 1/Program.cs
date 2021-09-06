1d24fb4cff92149e78dbfc7c258f783a0f287d0f
StasMyronenko <stasmyronenko96@gmail.com>
2021-09-06 09:40:35 +0300

    Добавьте файлы проекта.

---
+++ work 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first_work
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float y;
+            Console.WriteLine("Input X:");
+            String strX = Console.ReadLine();
+
+            float x = Convert.ToSingle(strX);
+            y = Convert.ToSingle(2 * Math.Pow(x, 4) - 3 * Math.Pow(x, 3) + +4 * Math.Pow(x, 2) - 5 * x + 6);
+            Console.WriteLine("y = " + y);
+
+            Console.ReadLine();
+        }
+    }
+}
